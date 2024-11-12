eccaf852b604267cd4a599912067589e38fc7925
Pipepatino <felipequimic99@gmail.com>
2024-11-12 14:55:11 -0500

    Eliminacion de comentarios

--- homework/word_count.py
+++ homework/word_count.py
@@ -10,10 +10,7 @@ import string
 import os
 import shutil
 
-# Prueba
-# Prueba 2
 
-#
 # Escriba la función load_input que recive como parámetro un folder y retorna
 # una lista de tuplas donde el primer elemento de cada tupla es el nombre del
 # archivo y el segundo es una línea del archivo. La función convierte a tuplas
